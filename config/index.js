61a55137cf125863939eac88228e4235f7711fa4
lordyan <73373552+lordyan-488@users.noreply.github.com>
2022-09-03 03:24:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oE5UL64ClUbOPKsTmmfUTFtNzO8w",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "qBYuOX-QKyBVDHSwbgpPoE3L0PScxfY-ocFTceNhGzI",
+      useTemplateId: "lq_HhsySK456N2itmyO8gi2rfLM85zG_2GVyvDhEC_Y",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://www.xzw.com/fortune/Gemini/"
     }, 
@@ -101,9 +101,9 @@ export const config = {
     // 在一起的日子
     {"keyword": "love_day", date: "2022-07-12"},
     // 退伍日, 不用可以删掉
-    {"keyword": "birthday1", date: "2023-06-02"},
+    {"keyword": "birthday1", date: "1998-06-02"},
     // sakana日
-    {"keyword": "birthday2", date: "2022-05-23"},
+    {"keyword": "birthday2", date: "1998-05-23"},
     // ... 
   ],
 
@@ -118,10 +118,12 @@ export const config = {
     //{"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
-      "因为太喜欢你，所以看谁都像是情敌。",
-      "申请成为你爱里的永久居民。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
+      "I copied the love words, but I love you really.",
+      "He never says tired, but he hopes you will accompany him.",
+      "Maybe it's just the right time. You have a smile around your eyes.",
+      "Miss, not wrong, but over.",
+      "A person, a city, a lifetime of heartache.",
+      "It's cold. I want to drink and care for you.",
     ]},
   ],
 
